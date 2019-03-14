87f8404f265d26541612d9a645fc916bcc38ea75
susy <susy@test.com>
2019-03-14 11:11:31 +0000

    ok

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -37,39 +37,56 @@ function markerInfo(name,ubication,phone,website){
   return html;
 }
 
+
 @Component({
   tag: 'app-map',
-styleUrl:'style.css'
+  styleUrl:'style.css'
 })
 export class Map {
+  //define any withou context changes
+  map: any;
+  //@State() map;
 
   @State() content= [];
+  @State() loading=false;
 
   @State()lat="";
   @State()long="";
 
-  @State() mapa;
+  @State() isLoading = true;
+
+  @State() markers;
+
+  componentDidLoad() {
+    this.map = L.map('map').locate({setView: true});
+    this.map.on('locationfound', e => {
+      this.lat = e.latitude;
+      this.long = e.longitude;
+      this.getQueryMap("bares",1);
+    });
+    this.map.setZoom(19);
+
+  }
 
   getQueryMap( newQuery, radius ){
 
-    var myIcon = L.icon({
-      iconUrl: 'https://img.icons8.com/nolan/64/000000/street-view.png',
-      iconSize: [40, 45],
-    });
+      var myIcon = L.icon({
+        iconUrl: 'https://img.icons8.com/nolan/64/000000/street-view.png',
+        iconSize: [40, 45],
+      });
 
-    var barIcon = L.icon({
-      iconUrl: 'https://img.icons8.com/nolan/64/000000/bar.png',
-      iconSize: [40, 45],
-    });
+      var barIcon = L.icon({
+        iconUrl: 'https://img.icons8.com/nolan/64/000000/bar.png',
+        iconSize: [40, 45],
+      });
 
-    var currentPosition = null;
-    var elementPosition = null;
+      var currentPosition = null;
+      var elementPosition = null;
 
-    const map = L.map('map').locate({setView: true}).on('locationfound', function(e){
-      this.lat = e.latitude;
-      this.long = e.longitude;
+      console.log("PASA");
 
-      currentPosition = L.marker([e.latitude,e.longitude], {icon: myIcon}).addTo(map);
+      currentPosition = L.marker([this.lat,this.long], {icon: myIcon}).addTo(this.map);
+        
       var html ='<div><h3>I am here</h3></div>'
       currentPosition.bindPopup(String(html));
 
@@ -86,7 +103,7 @@ export class Map {
       .then(data => {
         this.content = data;
         console.log(this.content);        
-        const results=this.content.results;
+        const results=data.results;
         results.map(result => {
         if (result.position !== undefined){
             var lat = result.position[0];
@@ -110,73 +127,83 @@ export class Map {
                 website = contact.website[0].value;
               }
 
-              elementPosition = L.marker([lat,long], {icon: barIcon}).addTo(map);
-
+              elementPosition = L.marker([lat,long], {icon: barIcon}).addTo(this.map);
+              
               var street =(location.street !== undefined ? location.street+",<br>" :"")+
               (location.house !== undefined ? location.house+"," :"")+
               (location.city !== undefined ? location.city :"");
 
               elementPosition.bindPopup(markerInfo(info.name,street,phone,website));
-
-
             });       
           }
         });
         
       });
-    })
 
-    L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png").addTo(map);
+    L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png").addTo(this.map);
 
-    map.on('popupopen', (e) => {
-      console.log('pop up open', e.popup);
+    this.map.on('popupopen', (/*e*/) => {
+      //console.log('pop up open', e.popup);
       document.getElementById('go').addEventListener('click', () => {
 
         var ubication = document.getElementById('ubication').textContent;
-
         location.href = "https://www.google.com/maps/search/?api=1&query="+ubication;
-
       }, false);
     })
-
-    this.mapa = map;
     
   }
 
-  componentDidLoad(){
-    this.getQueryMap("bares",1);
+  cleraAllLayers(){
+    this.map.eachLayer((layer) => {
+      this.map.removeLayer(layer);
+    });
   }
+
+  onRangeTouchEnd = () => {
+    this.loading = true;
+    this.cleraAllLayers();
+    
+    
+    var element = event.currentTarget as HTMLInputElement;
+    var value = element.value;
   
-  onChangeRange(){
-    alert("cambiado")
+    // var radious = parseInt(value) * 1000/100;
+    // if ( radious < 100){
+    //   radious = 100;
+    // }
+
+    if (parseInt(value) <= 300){
+      this.map.setZoom(18)
+    }else if( parseInt(value) > 300 && parseInt(value) <= 700){
+      this.map.setZoom(17)
+    }else if ( parseInt(value) > 700 ){
+      this.map.setZoom(16)
+    }
+
+
+    this.getQueryMap("bares",value);
+  };
+
+  renderIfLoading(){
+    if(this.loading === true){
+      console.log("SI")
+    }else{
+      console.log("NO")
+    }
   }
 
     render(){
         return [
-          <ion-header>       
-            <ion-item>
-              <ion-range id="myRange" onTouchEnd={()=>{
-                  
-                  var element = event.currentTarget as HTMLInputElement;
-                  var value = element.value;
-                
-                  var radious = parseInt(value) * 1000/100;
-                  if ( radious < 100){
-                    radious = 100;
-                  }
+          <ion-header>
+              <ion-toolbar>
+                <ion-title>
+                  <ion-range pin={true} min={100} max={1000} onTouchEnd={this.onRangeTouchEnd} >
+                    <ion-label slot="start">metros</ion-label>
+                  </ion-range>
 
-                  console.log(radious);
+                </ion-title>
+              </ion-toolbar>
 
-                  this.mapa.off();
-                  this.mapa.remove();
-
-                  this.getQueryMap("bares",radious);
-                  
-              }} color="secondary">
-                <ion-label slot="start">100m</ion-label>
-                <ion-label slot="end">1km</ion-label>
-              </ion-range>
-            </ion-item>
           </ion-header>,
           <ion-content>
           <div style={{
